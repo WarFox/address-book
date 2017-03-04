bc6aec0a0fdc5dd66b0eb933fdc8867a299260c6
WarFox (Deepu Mohan Puthrote) <git@deepumohan.com>
2017-03-04 17:19:00 +0000

    Fix tests for DateUtils daysBetween

--- src/main/java/com/gumtree/addressbook/utils/DateUtils.java
+++ src/main/java/com/gumtree/addressbook/utils/DateUtils.java
@@ -1,11 +1,12 @@
 package com.gumtree.addressbook.utils;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class DateUtils {
 
-    public static int daysBetween(LocalDate date1, LocalDate date2) {
-        return 0;
+    public static long daysBetween(LocalDate date1, LocalDate date2) {
+        return ChronoUnit.DAYS.between(date1, date2);
     }
 
 }
--- src/test/java/com/gumtree/addressbook/utils/DateUtilsTest.java
+++ src/test/java/com/gumtree/addressbook/utils/DateUtilsTest.java
@@ -12,7 +12,27 @@ public class DateUtilsTest {
     public void testDaysBetweenTodayAndYesterday() throws Exception {
         LocalDate today = LocalDate.now();
         LocalDate yesterday = today.minusDays(1);
-        assertThat(DateUtils.daysBetween(today, yesterday)).isEqualTo(1);
+        assertThat(DateUtils.daysBetween(yesterday, today)).isEqualTo(1);
+    }
+
+    @Test
+    public void testDaysBetween1stAnd3rdIs2() throws Exception {
+        LocalDate date1 = LocalDate.of(2016, 1,1);
+        LocalDate date2 = LocalDate.of(2016, 1,3);
+        assertThat(DateUtils.daysBetween(date1, date2)).isEqualTo(2);
+    }
+
+    @Test
+    public void testDaysBetweenSameDateShouldbeZero() throws Exception {
+        LocalDate today = LocalDate.now();
+        assertThat(DateUtils.daysBetween(today, today)).isEqualTo(0);
+    }
+
+    @Test
+    public void testWhenFirstDateIsGreaterThanSecondDate() throws Exception {
+        LocalDate date1 = LocalDate.of(2017, 1,1);
+        LocalDate date2 = LocalDate.of(2016, 1, 1);
+        assertThat(DateUtils.daysBetween(date1, date2)).isEqualTo(-366);
     }
 
 }
